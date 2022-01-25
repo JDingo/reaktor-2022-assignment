d4c72aeb3813cced5bb11ed9bfb708666eaeb8bf
JDingo <45366094+JDingo@users.noreply.github.com>
2022-01-25 17:18:27 +0200

    Add support for proxy

--- src/services/history.tsx
+++ src/services/history.tsx
@@ -1,8 +1,10 @@
 import { FetchObject, GameObject } from "../types";
 import axios from 'axios';
 
+const baseUrl = 'https://jdingo-reaktor-proxy.herokuapp.com';
+
 export const fetchMatchPage = async (cursorUrl: string): Promise<FetchObject> => {
-  const response = await axios.get<FetchObject>(cursorUrl);
+  const response = await axios.get<FetchObject>(process.env.NODE_ENV === 'development' ? cursorUrl : baseUrl + cursorUrl);
 
   const cursor: string = response.data.cursor;
   const data: Array<GameObject> = response.data.data;
